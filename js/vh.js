28d554f8f3b8869562e2f379bd8f1d3455cf34c4
konio <kotogatari@gmail.com>
2022-02-23 17:46:01 +0900

    modify vh.js

--- js/vh.js
+++ js/vh.js
@@ -1,9 +1,8 @@
-let vh = window.innerHeight * 0.01;
-
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-
+const setFillHeight = () => {
+  const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+};
+
+setFillHeight();
+
+window.addEventListener("resize", setFillHeight);
